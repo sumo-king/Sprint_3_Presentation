8a3795f754116c8a9017d5fe1eb115cf18c86d25
Shelton <stlosper@gmail.com>
2026-06-18 10:55:13 +0200

    feat: add Terraform configuration for AWS provider and outputs for EKS cluster

---
+++ terraform/outputs.tf
@@ -0,0 +1,35 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = module.eks.cluster_arn
+}
+
+output "cluster_certificate_authority_data" {
+  description = "EKS cluster certificate authority data"
+  value       = module.eks.cluster_certificate_authority_data
+  sensitive   = true
+}
+
+output "node_security_group_id" {
+  description = "Security group ID attached to the EKS worker nodes"
+  value       = module.eks.node_security_group_id
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "configure_kubectl" {
+  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,36 @@ variable "aws_region" {
   type    = string
   default = "af-south-1"
 }
+
+variable "project_name" {
+  type    = string
+  default = "ABSA touchpoint project"
+}
+
+
+variable "vpc_cidr" {
+  description = "CIDR range for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR ranges for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+}
+
+
+variable "private_subnet_cidrs" {
+  description = "CIDR ranges for private subnets"
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+}
+
+
